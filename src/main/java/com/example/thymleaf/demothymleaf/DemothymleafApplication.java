7dcf754c46f7892a28525d19e16f4279012b0185
Vishal Shelar <vishalshelar941@gmail.com>
2023-05-28 18:15:25 +0530

    initial commit

---
+++ src/main/java/com/example/thymleaf/demothymleaf/DemothymleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.thymleaf.demothymleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemothymleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemothymleafApplication.class, args);
+	}
+
+}
